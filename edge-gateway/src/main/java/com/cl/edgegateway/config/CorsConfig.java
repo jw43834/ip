86d75c8ecabe70c1148af212cff904606f61ccd0
Jade <jw43834@gmail.com>
2020-01-28 13:30:23 +0900

    add gateway cors filter

---
+++ edge-gateway/src/main/java/com/cl/edgegateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.cl.edgegateway.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Order(-999)
+@Configuration
+public class CorsConfig {
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean registration = new FilterRegistrationBean(new CorsFilter(source));
+        return registration;
+    }
+}
